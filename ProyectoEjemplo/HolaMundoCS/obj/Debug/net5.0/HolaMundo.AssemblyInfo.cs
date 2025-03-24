4a563489417d6dfdf6df93c411cf7eabfd65a838
DiegoJG <kokote888@users.noreply.github.com>
2025-03-24 12:14:40 +0100

    gitignore

--- ProyectoEjemplo/HolaMundoCS/obj/Debug/net5.0/HolaMundo.AssemblyInfo.cs
+++ ProyectoEjemplo/HolaMundoCS/obj/Debug/net5.0/HolaMundo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HolaMundo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29c6d880df3d3249551f85e6fe7d290f34d305f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HolaMundo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HolaMundo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
